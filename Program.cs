cc35a5832c2ac9dbc3a5daf2bd3251944c9f29f4
4452ankur <39676548+4452ankur@users.noreply.github.com>
2018-12-09 22:34:48 +0530

    Create Program.cs
    Start program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDP_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility utility = new Utility();
+            utility.Process_AllMethods();
+
+            Console.WriteLine("Thank u");
+            Console.ReadLine();
+        }
+    }
+}
